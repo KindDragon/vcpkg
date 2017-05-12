15ae02c7973c97d89749c821df60a0e70c0f280f
Arkady Shapkin <arkady.shapkin@gmail.com>
2017-05-12 23:01:47 +0300

    [openssl] Update version to 1.1.0e

--- ports/openssl/portfile.cmake
+++ ports/openssl/portfile.cmake
@@ -4,46 +4,46 @@ if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
 endif()
 
 include(vcpkg_common_functions)
-set(OPENSSL_VERSION 1.0.2k)
+set(OPENSSL_VERSION 1.1.0e)
 set(MASTER_COPY_SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/openssl-${OPENSSL_VERSION})
+
 vcpkg_find_acquire_program(PERL)
+vcpkg_find_acquire_program(NASM)
 find_program(NMAKE nmake)
 
 get_filename_component(PERL_EXE_PATH ${PERL} DIRECTORY)
-set(ENV{PATH} "${PERL_EXE_PATH};$ENV{PATH}")
+get_filename_component(NASM_EXE_PATH ${NASM} DIRECTORY)
+set(ENV{PATH} "${PERL_EXE_PATH};${NASM_EXE_PATH};$ENV{PATH}")
 
 vcpkg_download_distfile(OPENSSL_SOURCE_ARCHIVE
     URLS "https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz"
     FILENAME "openssl-${OPENSSL_VERSION}.tar.gz"
-	SHA512 0d314b42352f4b1df2c40ca1094abc7e9ad684c5c35ea997efdd58204c70f22a1abcb17291820f0fff3769620a4e06906034203d31eb1a4d540df3e0db294016
+    SHA512 4b75e925323703d4a31ade90ea687e027742b7bf4f0f6cb4476e7bb9e31dcaf60eb40d925bc768ff1c28ffa71b8f9639dd83662d49ad80100a388947e75647a0
 )
 
 vcpkg_extract_source_archive(${OPENSSL_SOURCE_ARCHIVE})
-vcpkg_apply_patches(
-    SOURCE_PATH ${MASTER_COPY_SOURCE_PATH}
-    PATCHES ${CMAKE_CURRENT_LIST_DIR}/PerlScriptSpaceInPathFixes.patch
-            ${CMAKE_CURRENT_LIST_DIR}/ConfigureIncludeQuotesFix.patch
-            ${CMAKE_CURRENT_LIST_DIR}/STRINGIFYPatch.patch
-)
 
 set(CONFIGURE_COMMAND ${PERL} Configure
     enable-static-engine
     enable-capieng
-    no-asm
-    no-ssl2
 )
 
 if(TARGET_TRIPLET MATCHES "x86-windows")
     set(OPENSSL_ARCH VC-WIN32)
-    set(OPENSSL_DO "ms\\do_ms.bat")
-elseif(TARGET_TRIPLET MATCHES "x64")
+elseif(TARGET_TRIPLET MATCHES "x64-windows")
     set(OPENSSL_ARCH VC-WIN64A)
-    set(OPENSSL_DO "ms\\do_win64a.bat")
 else()
     message(FATAL_ERROR "Unsupported target triplet: ${TARGET_TRIPLET}")
 endif()
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    set(OPENSSL_LINKAGE)
+else()
+    set(OPENSSL_LINKAGE no-shared)
+endif()
+
 file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg)
+
 
 message(STATUS "Build ${TARGET_TRIPLET}-rel")
 file(COPY ${MASTER_COPY_SOURCE_PATH} DESTINATION ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel)
@@ -51,28 +51,17 @@ set(SOURCE_PATH_RELEASE ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/openssl-
 set(OPENSSLDIR_RELEASE ${CURRENT_PACKAGES_DIR})
 
 vcpkg_execute_required_process(
-    COMMAND ${CONFIGURE_COMMAND} ${OPENSSL_ARCH} "--prefix=${OPENSSLDIR_RELEASE}" "--openssldir=${OPENSSLDIR_RELEASE}"
+    COMMAND ${CONFIGURE_COMMAND} ${OPENSSL_ARCH} ${OPENSSL_LINKAGE} "--prefix=${OPENSSLDIR_RELEASE}" "--openssldir=${OPENSSLDIR_RELEASE}"
     WORKING_DIRECTORY ${SOURCE_PATH_RELEASE}
     LOGNAME configure-perl-${TARGET_TRIPLET}-${CMAKE_BUILD_TYPE}-rel
 )
-vcpkg_execute_required_process(
-    COMMAND ${OPENSSL_DO}
-    WORKING_DIRECTORY ${SOURCE_PATH_RELEASE}
-    LOGNAME configure-do-${TARGET_TRIPLET}-${CMAKE_BUILD_TYPE}-rel
-)
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    vcpkg_execute_required_process(COMMAND ${NMAKE} -f ms\\ntdll.mak install
-                                   WORKING_DIRECTORY ${SOURCE_PATH_RELEASE}
-                                   LOGNAME build-${TARGET_TRIPLET}-rel)
-else()
-    vcpkg_execute_required_process(COMMAND ${NMAKE} -f ms\\nt.mak install
-                                   WORKING_DIRECTORY ${SOURCE_PATH_RELEASE}
-                                   LOGNAME build-${TARGET_TRIPLET}-rel)
-endif()
-
+vcpkg_execute_required_process(COMMAND ${NMAKE} install
+                               WORKING_DIRECTORY ${SOURCE_PATH_RELEASE}
+                               LOGNAME build-${TARGET_TRIPLET}-rel)
 
 message(STATUS "Build ${TARGET_TRIPLET}-rel done")
+
 
 message(STATUS "Build ${TARGET_TRIPLET}-dbg")
 file(COPY ${MASTER_COPY_SOURCE_PATH} DESTINATION ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg)
@@ -80,35 +69,42 @@ set(SOURCE_PATH_DEBUG ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/openssl-${
 set(OPENSSLDIR_DEBUG ${CURRENT_PACKAGES_DIR}/debug)
 
 vcpkg_execute_required_process(
-    COMMAND ${CONFIGURE_COMMAND} debug-${OPENSSL_ARCH} "--prefix=${OPENSSLDIR_DEBUG}" "--openssldir=${OPENSSLDIR_DEBUG}"
+    COMMAND ${CONFIGURE_COMMAND} debug-${OPENSSL_ARCH} ${OPENSSL_LINKAGE} "--prefix=${OPENSSLDIR_DEBUG}" "--openssldir=${OPENSSLDIR_DEBUG}"
     WORKING_DIRECTORY ${SOURCE_PATH_DEBUG}
     LOGNAME configure-perl-${TARGET_TRIPLET}-${CMAKE_BUILD_TYPE}-dbg
 )
-vcpkg_execute_required_process(
-    COMMAND ${OPENSSL_DO}
-    WORKING_DIRECTORY ${SOURCE_PATH_DEBUG}
-    LOGNAME configure-do-${TARGET_TRIPLET}-${CMAKE_BUILD_TYPE}-dbg
-)
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    vcpkg_execute_required_process(COMMAND ${NMAKE} -f ms\\ntdll.mak install
-                                   WORKING_DIRECTORY ${SOURCE_PATH_DEBUG}
-                                   LOGNAME build-${TARGET_TRIPLET}-dbg)
-else()
-    vcpkg_execute_required_process(COMMAND ${NMAKE} -f ms\\nt.mak install
-                                   WORKING_DIRECTORY ${SOURCE_PATH_DEBUG}
-                                   LOGNAME build-${TARGET_TRIPLET}-dbg)
-endif()
+vcpkg_execute_required_process(COMMAND ${NMAKE} install
+                               WORKING_DIRECTORY ${SOURCE_PATH_DEBUG}
+                               LOGNAME build-${TARGET_TRIPLET}-dbg)
 
 message(STATUS "Build ${TARGET_TRIPLET}-dbg done")
+
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE
     ${CURRENT_PACKAGES_DIR}/debug/bin/openssl.exe
     ${CURRENT_PACKAGES_DIR}/bin/openssl.exe
+    ${CURRENT_PACKAGES_DIR}/debug/bin/openssl.pdb
+    ${CURRENT_PACKAGES_DIR}/bin/openssl.pdb
+    ${CURRENT_PACKAGES_DIR}/debug/bin/ossl_static.pdb
+    ${CURRENT_PACKAGES_DIR}/bin/ossl_static.pdb
+    ${CURRENT_PACKAGES_DIR}/debug/openssl.cnf.dist
+    ${CURRENT_PACKAGES_DIR}/openssl.cnf.dist
     ${CURRENT_PACKAGES_DIR}/debug/openssl.cnf
     ${CURRENT_PACKAGES_DIR}/openssl.cnf
 )
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/certs)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/html)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/private)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/engines-1_1)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/misc)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/certs)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/html)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/private)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/engines-1_1)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/misc)
 
 file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/openssl RENAME copyright)
 
